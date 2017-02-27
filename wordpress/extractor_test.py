3b0e665535ce55e5c5b11aac7cef5b2c2638b997
Kristina Chodorow <k.chodorow@gmail.com>
2017-02-26 19:17:48 -0500

    Add limit test

--- wordpress/extractor_test.py
+++ wordpress/extractor_test.py
@@ -5,15 +5,26 @@ import unittest
 
 class ExtractorTest(unittest.TestCase):
   def test_extract(self):
-    with open('wordpress/test/snail.html', 'r') as fh:
-      index = fh.read()
-    page = BeautifulSoup(index, 'html.parser')
-    cache = TestCache()
-    extractor = Extractor(page, cache)
+    extractor = Extractor(
+      self._get_soup('wordpress/test/snail.html'), TestCache())
     urls = extractor.extract(0)
     self.assertEqual('https://www.example.com/title1', urls[0])
     self.assertEqual('https://www.example.com/title2', urls[1])
     self.assertEqual('https://www.example.com/title3', urls[2])
+
+  def test_limit(self):
+    extractor = Extractor(
+      self._get_soup('wordpress/test/snail.html'), TestCache())
+    urls = extractor.extract(2)
+    self.assertEqual(2, len(urls))
+    self.assertEqual('https://www.example.com/title1', urls[0])
+    self.assertEqual('https://www.example.com/title2', urls[1])
+
+  def _get_soup(self, path):
+    with open(path, 'r') as fh:
+      index = fh.read()
+    return BeautifulSoup(index, 'html.parser')
+
 
 class TestCache(object):
   def get(self, url):
